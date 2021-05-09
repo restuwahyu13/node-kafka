6dcf1b9f0f359b9e651a4f697cc773391c2da720
Restu Wahyu Saputra <restuwahyu13@gmail.com>
2021-05-09 22:58:37 +0700

    publish files

--- consumer.js
+++ consumer.js
@@ -3,7 +3,8 @@ const { kafkaConsumer } = require('./kafka')
 ;(async function () {
 	try {
 		const response = await kafkaConsumer('user-service-group', 'user-service')
-		consola.success(response.message.value.toString())
+		const result = JSON.parse(response.message.value.toString())
+		consola.success(`my name is ${result.name}`)
 	} catch (error) {
 		consola.info(chalk.red(error))
 	}
